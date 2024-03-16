46125ec2f37562691c58399c48a82eeb7c64d630
Juan Acevedo <jmamartinez18@gmail.com>
2024-03-16 17:00:36 -0600

    adding github link, adding working footer and images for projects

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer () {
+    return(
+        <section>
+            <a href='https://github.com/juanthtgotaway'><img src='../assets/projectimg/GitHub.png'></img></a>
+        </section>
+    )
+};
+
+export default Footer;
