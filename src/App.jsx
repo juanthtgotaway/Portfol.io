ecfa1aeee534b01e1db96cfef649c8a37199e928
Juan Acevedo <jmamartinez18@gmail.com>
2024-03-16 10:31:16 -0600

    adding app and main jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Projects from './components/projects';
+
+function App() {
+  return (
+    <div className="Portfol.io">
+      <Projects />
+    </div>
+  );
+}
+
+export default App;
