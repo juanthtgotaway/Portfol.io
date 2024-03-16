831bfcfdbdc279a00e2394a328938f7e1bb43f09
Juan Acevedo <jmamartinez18@gmail.com>
2024-03-16 17:04:56 -0600

    created structure in nav bar/header

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -4,40 +4,47 @@ function NavTabs() {
     const currentPage = useLocation().pathname;
 
     return(
-        <ul>
-            <li className="nav nav-tabs">
-                <Link
-                    to="/"
-                    className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
-                >
-                    About
-                </Link>
-            </li>
-            <li className="nav nav-tabs">
-                <Link
-                    to="/Projects"
-                    className={currentPage === '/Projects' ? 'nav-link active' : 'nav-link'}
-                >
-                    Projects
-                </Link>
-            </li>
-            <li className="nav nav-tabs">
-                <Link
-                    to="/Contact"
-                    className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
-                >
-                    Contact
-                </Link>
-            </li>
-            <li className="nav nav-tabs">
-                <Link
-                    to="/Resume"
-                    className={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}
-                >
-                    Resume
-                </Link>
-            </li>
-        </ul>
+        <div className="header">
+            <div>
+                <h1>JuanThtGotAway</h1>
+            </div>
+            <div className="menu">
+                <ul>
+                    <li className="nav nav-tabs">
+                        <Link
+                            to="/"
+                            className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
+                        >
+                            About
+                        </Link>
+                    </li>
+                    <li className="nav nav-tabs">
+                        <Link
+                            to="/Projects"
+                            className={currentPage === '/Projects' ? 'nav-link active' : 'nav-link'}
+                        >
+                            Projects
+                        </Link>
+                    </li>
+                    <li className="nav nav-tabs">
+                        <Link
+                            to="/Contact"
+                            className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
+                        >
+                            Contact
+                        </Link>
+                    </li>
+                    <li className="nav nav-tabs">
+                        <Link
+                            to="/Resume"
+                            className={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}
+                        >
+                            Resume
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
     );
 }
 
