d07b12542e4684ceb1bd7ce991fe27d81169d7fc
Juan Acevedo <jmamartinez18@gmail.com>
2024-03-17 18:27:37 -0600

    cleared up dead code

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -11,7 +11,7 @@ function NavTabs() {
     return (
         <>
             {['md'].map((expand) => (
-                <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3">
+                <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3" data-bs-theme="dark">
                     <Container fluid>
                         <Navbar.Brand href="/">
                             <h1>Juan Martinez</h1>
@@ -63,55 +63,6 @@ function NavTabs() {
                 </Navbar>
             ))}
         </>
-
-        // <div>
-        //     <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        //         <div className="container-fluid">
-        //             <span className="navbar-brand">
-        //                 <h1>Juan Martinez</h1>
-        //             </span>
-        //             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        //                 <span className="navbar-toggler-icon"></span>
-        //             </button>
-        //         </div>
-        //         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-        //             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        //                 <li className="nav-item">
-        //                     <Link
-        //                         to="/"
-        //                         className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
-        //                     >
-        //                         About
-        //                     </Link>
-        //                 </li>
-        //                 <li className="nav-item">
-        //                     <Link
-        //                         to="/Projects"
-        //                         className={currentPage === '/Projects' ? 'nav-link active' : 'nav-link'}
-        //                     >
-        //                         Projects
-        //                     </Link>
-        //                 </li>
-        //                 <li className="nav-item">
-        //                     <Link
-        //                         to="/Contact"
-        //                         className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
-        //                     >
-        //                         Contact
-        //                     </Link>
-        //                 </li>
-        //                 <li className="nav-item">
-        //                     <Link
-        //                         to="/Resume"
-        //                         className={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}
-        //                     >
-        //                         Resume
-        //                     </Link>
-        //                 </li>
-        //             </ul>
-        //         </div>
-        //     </nav>
-        // </div>
     );
 }
 
