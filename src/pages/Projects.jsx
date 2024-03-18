60bb9db58a21b65fc26aeae93fb002f3a1a8ef22
Juan Acevedo <jmamartinez18@gmail.com>
2024-03-17 18:43:00 -0600

    added touches to the styles

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -8,7 +8,7 @@ export default function Projects() {
           <a href='https://notehaven.onrender.com/'>
             <Card.Img className='card-img-grayscale' src="../assets/projectimg/NoteHaven.png" alt="Card image" />
             <Card.ImgOverlay  >
-              <Card.Title style={{color: 'white'}}>Note Haven</Card.Title>
+              <Card.Title className='cardTitle' style={{color: 'white'}}>Note Haven</Card.Title>
             </Card.ImgOverlay>
           </a>
         </Card>
@@ -17,7 +17,7 @@ export default function Projects() {
           <a href='https://workplace-management-app-ad243e4e571c.herokuapp.com/'>
             <Card.Img className='card-img-grayscale' src="../assets/projectimg/WorkplaceMngr.png" alt="Card image" />
             <Card.ImgOverlay>
-              <Card.Title style={{color: 'white'}}>Workplace Manager</Card.Title>
+              <Card.Title className='cardTitle' style={{color: 'white'}}>Workplace Manager</Card.Title>
             </Card.ImgOverlay>
           </a>
         </Card>
@@ -26,7 +26,7 @@ export default function Projects() {
           <a href='https://ponderpad-dd01cc8d291c.herokuapp.com/'>
             <Card.Img className='card-img-grayscale'src="../assets/projectimg/PonderPad.png" alt="Card image" />
             <Card.ImgOverlay>
-              <Card.Title style={{color: 'black'}}>Ponder Pad</Card.Title>
+              <Card.Title className='cardTitle'style={{color: 'black'}}>Ponder Pad</Card.Title>
             </Card.ImgOverlay>
           </a>
         </Card>
@@ -35,7 +35,7 @@ export default function Projects() {
           <a href='https://juanthtgotaway.github.io/Weather-Website/'>
             <Card.Img className='card-img-grayscale'src="../assets/projectimg/Weatherapp.png" alt="Card image" />
             <Card.ImgOverlay>
-              <Card.Title style={{color: 'white'}}>Weather App</Card.Title>
+              <Card.Title className='cardTitle' style={{color: 'white'}}>Weather App</Card.Title>
             </Card.ImgOverlay>
           </a>
         </Card>
@@ -44,7 +44,7 @@ export default function Projects() {
           <a href='https://juanthtgotaway.github.io/Book-App/'>
             <Card.Img className='card-img-grayscale'src="../assets/projectimg/Book_Years.png" alt="Card image" />
             <Card.ImgOverlay>
-              <Card.Title style={{color: 'black'}}>Book Years</Card.Title>
+              <Card.Title className='cardTitle' style={{color: 'white'}}>Book Years</Card.Title>
             </Card.ImgOverlay>
           </a>
         </Card>
@@ -53,7 +53,7 @@ export default function Projects() {
           <a href='https://juanthtgotaway.github.io/coding-questionnaire/'>
             <Card.Img className='card-img-grayscale'src="../assets/projectimg/CodingQuiz.png" alt="Card image" />
             <Card.ImgOverlay>
-              <Card.Title style={{color: 'black'}}>Coding Quiz</Card.Title>
+              <Card.Title className='cardTitle' style={{color: 'white'}}>Coding Quiz</Card.Title>
             </Card.ImgOverlay>
           </a>
         </Card>
