6a1186b566a55764ac159caaf49f8f3fd1b4533c
Juan Acevedo <jmamartinez18@gmail.com>
2024-04-20 17:40:35 -0600

    trying another way to load images

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,13 +1,21 @@
 import '../App.css'
 import Card from 'react-bootstrap/Card';
+import NoteHavenImg from '../../assets/projectimg/NoteHaven.png';
+import WorkplaceMngrImg from '../../assets/projectimg/WorkplaceMngr.png';
+import PonderPadImg from '../../assets/projectimg/PonderPad.png';
+import WeatherappImg from '../../assets/projectimg/Weatherapp.png';
+import Book_YearsImg from '../../assets/projectimg/Book_Years.png';
+import CodingQuizImg from '../../assets/projectimg/CodingQuiz.png';
+import RMVGImg from '../../assets/projectimg/RMVG.png';
 
 export default function Projects() {
     return (
       <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around', gap: '10px'}}>
+        
         <Card className="bg-dark text-white" style={{width: "400px"}}>
           <a href='https://notehaven.onrender.com/'>
-            <Card.Img className='card-img-grayscale' src="../../assets/projectimg/NoteHaven.png" alt="Card image" />
-            <Card.ImgOverlay  >
+            <Card.Img className='card-img-grayscale' src={NoteHavenImg} alt="Note Haven Project Image" />
+            <Card.ImgOverlay>
               <Card.Title className='cardTitle' style={{color: 'white'}}>Note Haven</Card.Title>
             </Card.ImgOverlay>
           </a>
@@ -15,7 +23,7 @@ export default function Projects() {
 
         <Card className="bg-dark text-white" style={{width: "400px"}}>
           <a href='https://workplace-management-app-ad243e4e571c.herokuapp.com/'>
-            <Card.Img className='card-img-grayscale' src="../../assets/projectimg/WorkplaceMngr.png" alt="Card image" />
+            <Card.Img className='card-img-grayscale' src={WorkplaceMngrImg} alt="Workplace Manager Project Image" />
             <Card.ImgOverlay>
               <Card.Title className='cardTitle' style={{color: 'white'}}>Workplace Manager</Card.Title>
             </Card.ImgOverlay>
@@ -24,16 +32,16 @@ export default function Projects() {
 
         <Card className="bg-dark text-white" style={{width: "400px"}}>
           <a href='https://ponderpad-dd01cc8d291c.herokuapp.com/'>
-            <Card.Img className='card-img-grayscale'src="../../assets/projectimg/PonderPad.png" alt="Card image" />
+            <Card.Img className='card-img-grayscale' src={PonderPadImg} alt="Ponder Pad Project Image" />
             <Card.ImgOverlay>
-              <Card.Title className='cardTitle'style={{color: 'black'}}>Ponder Pad</Card.Title>
+              <Card.Title className='cardTitle' style={{color: 'black'}}>Ponder Pad</Card.Title>
             </Card.ImgOverlay>
           </a>
         </Card>
 
         <Card className="bg-dark text-white" style={{width: "400px"}}>
           <a href='https://juanthtgotaway.github.io/Weather-Website/'>
-            <Card.Img className='card-img-grayscale'src="../../assets/projectimg/Weatherapp.png" alt="Card image" />
+            <Card.Img className='card-img-grayscale' src={WeatherappImg} alt="Weather App Project Image" />
             <Card.ImgOverlay>
               <Card.Title className='cardTitle' style={{color: 'white'}}>Weather App</Card.Title>
             </Card.ImgOverlay>
@@ -42,7 +50,7 @@ export default function Projects() {
         
         <Card className="bg-dark text-white" style={{width: "400px"}}>
           <a href='https://juanthtgotaway.github.io/Book-App/'>
-            <Card.Img className='card-img-grayscale'src="../../assets/projectimg/Book_Years.png" alt="Card image" />
+            <Card.Img className='card-img-grayscale' src={Book_YearsImg} alt="Book Years Project Image" />
             <Card.ImgOverlay>
               <Card.Title className='cardTitle' style={{color: 'black'}}>Book Years</Card.Title>
             </Card.ImgOverlay>
@@ -51,7 +59,7 @@ export default function Projects() {
 
         <Card className="bg-dark text-white" style={{width: "400px"}}>
           <a href='https://juanthtgotaway.github.io/coding-questionnaire/'>
-            <Card.Img className='card-img-grayscale'src="../../assets/projectimg/CodingQuiz.png" alt="Card image" />
+            <Card.Img className='card-img-grayscale' src={CodingQuizImg} alt="Coding Quiz Project Image" />
             <Card.ImgOverlay>
               <Card.Title className='cardTitle' style={{color: 'white'}}>Coding Quiz</Card.Title>
             </Card.ImgOverlay>
@@ -60,7 +68,7 @@ export default function Projects() {
 
         <Card className="bg-dark text-white" style={{width: "400px"}}>
           <a href='https://github.com/juanthtgotaway/rockymountainnationalparkvisitorsguide'>
-            <Card.Img className='card-img-grayscale'src="../../assets/projectimg/RMVG.png" alt="Card image" />
+            <Card.Img className='card-img-grayscale' src={RMVGImg} alt="Rocky Mountain Visitor Guide Project Image" />
             <Card.ImgOverlay>
               <Card.Title className='cardTitle' style={{color: 'white'}}>RockyMountainVisitor</Card.Title>
             </Card.ImgOverlay>
