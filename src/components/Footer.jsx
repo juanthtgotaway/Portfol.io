4a7fd32ce749399ce2e54f4410f7cc65dd8a573a
Juan Acevedo <jmamartinez18@gmail.com>
2024-05-15 09:53:43 -0600

    added bootcamp badge

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import '../App.css'
 import GitHub from '../../assets/projectimg/GitHub.png';
-
+import BtCmpBdg from '../../assets/projectimg/BadgeBootcamp.png'
 
 function Footer() {
     return (
@@ -9,7 +9,11 @@ function Footer() {
                 <div className="container-sm subDiv">
                     <a href='https://github.com/juanthtgotaway'><img src={GitHub} className="logo"></img></a>
                 </div>
+                <div className="container-sm subDiv">
+                    <a href='https://www.credly.com/badges/ff70318d-5009-4d05-a27e-3873b9fe8b57/public_url'><img src={BtCmpBdg} className="logo"></img></a>
+                </div>
             </div>
+            
         </section>
     )
 };
